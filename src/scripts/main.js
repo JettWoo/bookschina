93f7714a82b7b0f40138cab7d46adf4df97fc81a
Jett.Woo <jett.woo@qq.com>
2020-01-14 22:03:51 +0800

    修改了首页的一些效果

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,18 @@ import '../stylesheets/index.css';
 import '../stylesheets/cartlist.css';
 import '../stylesheets/detail.css';
 import '../stylesheets/register.css';
+
+import 'jquery';
+import '../scripts/bootstrap.min.js'
+import {
+    Swiper
+} from "./banner.js";
+
+new Swiper().init();
+
+$(".certifica").load("./footer.html", function () {});
+
+$("#tabSwitch a").hover(function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+});
